549c7d638d4b0f4d55015cd6703eb85a71318148
Pete James <petejames@Petes-MacBook-Air.local>
2020-02-15 10:39:05 -0800

    friend list loading

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from '../api/axiosAuth';
 
 
 // import Loader from 'react-loader-spinner';
 
+
 const FriendsList = () => {
 
-    axiosWithAuth().get('http://localhost:5000/api/friends').then(res => console.log('friendlist res with auth', res));
+    let styles = {
+        margin: '20px',
+        padding: '20px',
+        width: '1000px',
+        height: '180px',
+        backgroundColor: '#88CCFF',
+      };
 
+    const [friendArray, setFriendArray] = useState([]);
 
-        // axios
-        //     .get('http://localhost:5000/api/friends', token)
-        //     .then(res => {
-        //         console.log('friends response',res);
-        //       })
-        //       .catch(err => {
-        //         console.log("invalid login: ", err);
-        //       });
+    useEffect(() => {
+        axiosWithAuth().get('http://localhost:5000/api/friends')
+        .then(res => {
+            console.log('friendlist res with auth', res);
+            setFriendArray(res.data)
+    })
+
+      }, []);
+
+    // function loadFriends() {
+
+    //         console.log('friendArray from loadFriends', friendArray);
+
+    //         var result = friendArray.map(item => (
+    //             <div className="friend">
+    //             <h2>{item.name}</h2>
+    //             <h3>Age: {item.age}</h3>
+    //             <h3>Email: {item.email}</h3>
+    //             </div>
+    //         ))
+    //         return result
+    // }
 
     return (
-    <div className='friendlist'>
+    <div className='app'>
         <h1>Friend List</h1>
-
-
+        <div className='content'>
+        {friendArray.map(item=>(
+            <div className='friend-item' style={styles}>
+            <h2>Name: {item.name}</h2>
+            <h3>Age: {item.age}</h3>
+            <h3>Email: {item.email}</h3>
+            </div>
+        ))}
+        </div>
     </div>
     )
 }
